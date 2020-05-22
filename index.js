9ac357b6da733745ede9f71cbc725a61a6a8ed71
Matheus Giacomini <matheus.dua@hotmail.com>
2020-05-22 09:47:38 -0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('./models/Users');
+require('./models/users');
 
 mongoose.connect(keys.mongoURI);
 
