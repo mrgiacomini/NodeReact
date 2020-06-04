fbd25b790bc06746d57f5db9049022f4fcddf922
Matheus Giacomini <matheus.dua@hotmail.com>
2020-06-04 18:05:14 -0300

    mudando lista da home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Content } from "../styles";
-import { Container, Button, List, ListItem, ListItemText, Card, CardContent, Divider } from '@material-ui/core';
+import { Container, Button, List, ListItem, ListItemText, Card, CardContent, CardActionArea, Divider, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import Service from '../services/service';
 
 class Home extends Component {
     state = {
-      clientList: []
+      clientList: [],
+      didGetClientes: false
     };
 
     componentDidMount() {
       Service.getClients().then(response => {
-        this.setState({clientList: response.data});
+        this.setState({clientList: response.data, didGetClientes: true });
       });
     }
 
@@ -26,7 +27,7 @@ class Home extends Component {
           <Content>               
             <Button component={Link} to={'/cadastro'} variant="contained" color="primary">Novo</Button>
             <Content/>
-            <Card> 
+            {/* <Card> 
               <CardContent>
                 <List> 
                 {this.state.clientList.map((client, indice) => (                 
@@ -38,19 +39,44 @@ class Home extends Component {
                       to={{ 
                         pathname: '/cadastro',
                         state: client
-                      }} >
+                      }} 
+                      style={{borderRadius: 4}}>
                         <ListItemText
                           primary={client.name}
                           secondary={'R$ '+ Number(client.totalAmount).toLocaleString('pt-BR')}
+                          leftSubtitle={"esquerda"}
+                          rightSubtitle={"Direita"}
+                          chevron={{ color: 'white' }}
                         />
                       </ListItem>                   
                     </div>
                 )
                 )}
-                {!this.state.clientList.length && <ListItem><ListItemText primary="Sem clientes por enquanto."/></ListItem>}
+                {!this.state.clientList.length && this.state.didGetClientes 
+                  && <ListItem><ListItemText primary="Sem clientes por enquanto."/></ListItem>}
                 </List>
               </CardContent>
-            </Card>
+            </Card> */}
+            {this.state.clientList.map((client, indice) => (
+				<Card key={client._id} style={{marginBottom:10}}>
+					<CardActionArea 
+                      component={Link} 
+                      to={{ 
+                        pathname: '/cadastro',
+                        state: client
+                      }} >
+						<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{client.name}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							R$ {Number(client.totalAmount).toLocaleString('pt-BR')}
+						</Typography>
+						</CardContent>
+                  	</CardActionArea>
+              	</Card>
+				)	 
+			)}
           </Content>
         </Container>
       );
