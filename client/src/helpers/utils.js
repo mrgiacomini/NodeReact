e970d4e4f84293e2ff6ce0a86adc5a62ac1db427
Matheus Ramos Giacomini <matheus.dua@hotmail.com>
2021-10-19 16:05:31 -0300

    removendo frontend

--- client/src/helpers/utils.js
+++
@@ -1,5 +0,0 @@
-var days = ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado'];
-
-export const dayOfWeek = (day) => {
-    return days[day];
-}
--- client/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-class LoginService {
-    login(facebookData) {
-        return axios.post(process.env.REACT_APP_SERVER+'/facebooklogin', {userID: facebookData.userID, accessToken: facebookData.accessToken});
-    }
-}
-
-export default new LoginService();
